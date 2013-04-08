fa88b352b23db0286cd946250f82c1d6da709880
Jacob Mather <jmather@jmather.com>
2013-04-08 00:37:39 -0700

    Minor ordering alteration

--- puppet/manifests/phpwebdev.pp
+++ puppet/manifests/phpwebdev.pp
@@ -1,11 +1,11 @@
 # Puppet manifest for my PHP dev machine
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 class phpdevweb{
+	require server::misc
 	require server::ius
 	require server::yum
 	include server::iptables
 	#include rpmforge
-	include server::misc
 	include server::httpd
 	include server::phpdev
 	include server::db
--- puppet/modules/server/manifests/yum.pp
+++ puppet/modules/server/manifests/yum.pp
@@ -1,7 +1,23 @@
 class server::yum {
-  require server::ius
+  require server::yum::ius
+
+  exec { "grap-epel":
+    command => "/bin/rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm",
+    creates => "/etc/yum.repos.d/epel.repo",
+    alias   => "grab-epel",
+  }
+
   exec { 'yum-update':
     command => '/usr/bin/yum -y update',
     timeout => 900
   }
+}
+
+class server::yum::ius {
+   yumrepo { "IUS":
+      baseurl => "http://dl.iuscommunity.org/pub/ius/stable/$operatingsystem/6/$architecture",
+      descr => "IUS Community repository",
+      enabled => 1,
+      gpgcheck => 0
+   }
 }
